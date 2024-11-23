0cb6b40d51525df282b9978e02800cfa46d249c9
yaseen_mirshal <151372491+yaseenmirshal@users.noreply.github.com>
2024-11-23 15:21:14 +0530

    arranged in branch

--- app/Branch/page.jsx
+++ app/Branch/page.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const Branches = () => {
   useEffect(() => {
@@ -16,7 +17,7 @@ const Branches = () => {
     <>
       <Navbar />
       <div className="bg-gray-900 text-white min-h-screen">
-        <div className="container mx-auto p-6 space-y-16 pt-52">
+        <div className="container mx-auto p-6 space-y-16 pt-36 md:pt-52">
           {/* Branch 1 Section */}
           <div className="space-y-8" data-aos="fade-up">
             {/* Location Name */}
@@ -31,31 +32,31 @@ const Branches = () => {
             <div className="flex flex-col lg:flex-row items-center space-y-6 lg:space-y-0 lg:space-x-6">
               {/* Video */}
               <div className="lg:w-1/2 relative" data-aos="fade-right">
-  <video
-    autoPlay
-    loop
-    // muted
-    className="w-full rounded-lg shadow-lg object-cover"
-  >
-    <source src="/Saloon.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-</div>
+                <video
+                  autoPlay
+                  loop
+                  // muted
+                  className="w-full rounded-lg shadow-lg object-cover"
+                >
+                  <source src="/branch1.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
 
               {/* Images */}
               <div
-                className="lg:w-1/2 grid grid-cols-2 gap-4"
-                data-aos="fade-left"
+                className="lg:w-1/2 grid  gap-6"
+                data-aos="fade-right"
               >
                 <img
                   src="/branch1.jpeg"
                   alt="Branch 1 Image 1"
-                  className="rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
+                  className="w-full h-48 md:h-[520px] object-cover rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
                 />
                 <img
-                   src="/3 team1.jpeg"
+                  src="/3 team1.jpeg"
                   alt="Branch 1 Image 2"
-                  className="rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
+                  className="w-full h-48 md:h-[520px] object-cover rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
                 />
               </div>
             </div>
@@ -73,40 +74,40 @@ const Branches = () => {
 
             {/* Content Section */}
             <div className="flex flex-col lg:flex-row items-center space-y-6 lg:space-y-0 lg:space-x-6">
-             {/* Video */}
-             <div className="lg:w-1/2 relative" data-aos="fade-right">
-  <video
-    autoPlay
-    loop
-    muted
-    className="w-full rounded-lg shadow-lg object-cover"
-  >
-    <source src="/Saloon.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-</div>
-
+              {/* Video */}
+              <div className="lg:w-1/2 relative" data-aos="fade-right">
+                <video
+                  autoPlay
+                  loop
+                  muted
+                  className="w-full rounded-lg shadow-lg object-cover"
+                >
+                  <source src="/Saloon.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
 
               {/* Images */}
               <div
-                className="lg:w-1/2 grid grid-cols-2 gap-4"
-                data-aos="fade-left"
+                className="lg:w-1/2 grid  gap-6"
+                data-aos="fade-down"
               >
                 <img
                   src="/branch2.jpeg"
                   alt="Branch 2 Image 1"
-                  className="rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
+                  className="w-full h-48 md:h-[520px] object-cover rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
                 />
                 <img
                   src="/3team.jpeg"
                   alt="Branch 2 Image 2"
-                  className="rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
+                  className="w-full h-48 md:h-[520px] object-cover rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
                 />
               </div>
             </div>
           </div>
         </div>
       </div>
+      <Footer/>
     </>
   );
 };
