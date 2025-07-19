6d9ecddf9b8fb8fd391023fce5dab30d51e3c2eb
yaseenmirshal <yaseenmirshal5@gmail.com>
2025-07-19 21:17:43 +0530

    updated meta

--- app/layout.js
+++ app/layout.js
@@ -26,6 +26,24 @@ export const metadata = {
 export default function RootLayout({ children, pageProps }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Stylicle Salon</title>
+        <meta name="description" content="Discover your own style" />
+        <link rel="canonical" href="https://www.stylicle.com/" />
+        {/* Open Graph tags */}
+        <meta property="og:title" content="Stylicle Salon" />
+        <meta property="og:description" content="Discover your own style" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.stylicle.com/" />
+        <meta property="og:image" content="/public/styliclogo.png" />
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Stylicle Salon" />
+        <meta name="twitter:description" content="Discover your own style" />
+        <meta name="twitter:image" content="/public/styliclogo.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
